c4e1334b110e4eff9dd28b59d4fda458fe3fac6a
youable <youable.framios@gmail.com>
2024-07-01 23:46:01 +0900

    feat: init project

---
+++ src/main/java/org/youable/margins/IntendedMarginsSpringApplication.java
@@ -0,0 +1,13 @@
+package org.youable.margins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntendedMarginsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntendedMarginsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/youable/margins/IntendedMarginsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youable.margins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntendedMarginsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
